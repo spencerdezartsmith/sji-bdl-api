1457516d0d1e55daded71315e7d4a8cca4201d30
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2017-05-24 11:38:29 -0700

    Import routes file into app. Connect the two

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const express = require('express');
+const routes = require('./routes/routes');
 const app = express();
 
-app.get('/api', (req, res) => {
-  res.send({ hello: 'world' });
-});
+routes(app);
 
 module.exports = app;

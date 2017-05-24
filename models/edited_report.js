e1d0ab8c3f6bd67daad0521b305cad0ae6e2b2b5
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2017-05-24 12:34:30 -0700

    Build EditedReport Model

---
+++ models/edited_report.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const EditedReportSchema = new Schema({
+	title: {
+		type: String,
+		required: true
+	},
+	content: {
+		type: String,
+		required: true
+	},
+	geolocation: PointSchema,
+	report: { type: Schema.Types.ObjectId, ref: 'report' }
+});
+
+const EditedReport = mongoose.model('editedReport', EditedReportSchema);
+
+module.exports = EditedReport;

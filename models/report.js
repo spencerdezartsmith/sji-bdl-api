38d6f5583fefdaae83682426c20060d5b05ec03f
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2017-05-24 12:13:49 -0700

    Build out Report Model

---
+++ models/report.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ReportSchema = new Schema({
+	city: {
+		type: String,
+		required: true
+	},
+	locationType: {
+		type: String,
+		required: true
+	},
+	geolocation: PointSchema,
+	gender: String,
+	date: String,
+	assaultType: [String],
+	assaultDescription: String,
+	perpetrator: PerpSchema,
+	editedReport: { type: Schema.Types.ObjectId, ref: 'editedReport'}
+});
+
+const Report = mongoose.model('report', ReportSchema);
+
+module.exports = Report;

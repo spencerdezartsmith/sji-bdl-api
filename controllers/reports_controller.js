7b99180b2de40c90998f5fe5e6c4fe10cfab9bde
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2017-05-24 18:27:29 -0700

    Create userCreateReport function

--- controllers/reports_controller.js
+++ controllers/reports_controller.js
@@ -1,5 +1,16 @@
+const Report = require('../models/report');
+const EditedReport = require('../models/edited_report');
+
 module.exports = {
 	greeting(req, res) {
 		res.send({ hello: 'world' });
+	},
+
+	userCreateReport(req, res, next) {
+		const reportProps = req.body;
+
+    Report.create(reportProps)
+      .then(report => res.send(report))
+      .catch(next)
 	}
 };

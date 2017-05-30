2c4449c920f13cd033cbca7d9fdc717efbd759e6
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2017-05-30 14:11:12 -0700

    Add login route POST /api/admins/login

--- controllers/admins_controller.js
+++ controllers/admins_controller.js
@@ -1,5 +1,6 @@
 const _ = require('lodash');
 const Admin = require('../models/admin');
+const bcrypt = require('bcryptjs');
 
 module.exports = {
   createNewAdmin(req, res, next) {
@@ -11,5 +12,24 @@ module.exports = {
         res.header('x-auth', token).send(admin)
       })
       .catch(next);
+  },
+
+  login(req, res, next) {
+    const loginProps = _.pick(req.body, ['email', 'password']);
+
+    Admin.findByCredentials(loginProps.email, loginProps.password)
+      .then((admin) => {
+        return admin.generateAuthToken()
+          .then((token) => {
+            res.header('x-auth', token).send(admin);
+          })
+      })
+      .catch((error) => {
+        res.status(400).send();
+      });
+  },
+
+  testing(req, res, next) {
+    res.send(req.admin)
   }
 };

6256985a4b4499916e9333405a6b90c18c814c98
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2017-05-31 16:25:32 -0700

    Switch port variable

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const app = require('./app');
 
-app.listen(3000, () => console.log('Server is listening on port 3000'));
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Server is listening on port ${port}`));

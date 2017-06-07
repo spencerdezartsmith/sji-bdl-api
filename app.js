28c3c7e61976691fca2af4fb0855b1d343d01453
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2017-06-06 17:38:06 -0700

    Set up access to custom auth header

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const allowCrossDomain = (req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  res.header('Access-Control-Expose-Headers', 'x-auth')
 
   // intercept OPTIONS method
   if ('OPTIONS' == req.method) {
